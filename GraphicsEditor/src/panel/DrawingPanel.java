8cc4ae0e5548767f53c32c281c30e745064b171a
choi.mjuclass <choi.mjuclass@Notebook-HP.LSIS.BIZ>
2017-03-15 20:47:12 +0900

    toolbox

---
+++ GraphicsEditor/src/panel/DrawingPanel.java
@@ -0,0 +1,14 @@
+package panel;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class DrawingPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	
+	public DrawingPanel() {
+		this.setBackground(Color.WHITE);
+	}
+	
+}
