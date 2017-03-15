8eb5fbfd5a313cee8d0fcbc6a09cf6642a795c85
choi.mjuclass <choi.mjuclass@Notebook-HP.LSIS.BIZ>
2017-03-15 20:54:56 +0900

    initial

---
+++ GraphicsEditorOld/src/shapes/CHeartManager.java
@@ -0,0 +1,34 @@
+package shapes;
+
+import java.awt.Rectangle;
+
+public class CHeartManager extends CShapeManager {
+
+	private static final long serialVersionUID = 1L;
+	
+	public CHeartManager() {
+		super(EDrawingType.TwoPoint);
+		this.shape = new Rectangle();
+	}
+
+	@Override
+	public void setOrgin(int x, int y) {
+		Rectangle rectangle = (Rectangle) this.shape;
+		rectangle.setLocation(x, y);
+		rectangle.setSize(0, 0);
+	}
+	@Override
+	public void movePoint(int x, int y) {
+		Rectangle rectangle = (Rectangle) this.shape;
+		rectangle.setSize(x-rectangle.x, y-rectangle.y);
+	}
+	@Override
+	public void addPoint(int x, int y) {		
+	}
+
+	@Override
+	public CShapeManager newClone() {
+		// TODO Auto-generated method stub
+		return new CHeartManager();
+	}
+}
