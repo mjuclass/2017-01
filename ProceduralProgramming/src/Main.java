485232ec3e07b530a73a7cdfe8bb7c5e4bd449a2
choi.mjuclass <choi.mjuclass@Notebook-HP.LSIS.BIZ>
2017-03-26 18:31:06 +0900

    2017-03-26

--- ProceduralProgramming/src/Main.java
+++ ProceduralProgramming/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 
+
 public class Main {
 	public static void main(String[] args) {
 		try {
@@ -19,8 +20,7 @@ public class Main {
 					intInput != 0x20;
 					intInput = System.in.read()) {
 				System.out.println(intInput);			
-			}
-		
+			}		
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
