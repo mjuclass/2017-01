91728cb54a1f508b6502e2c6312391736100fd0a
choi.mjuclass <choi.mjuclass@Notebook-HP.LSIS.BIZ>
2017-03-27 08:35:18 +0900

    2017-03-26

---
+++ GraphicEditorOld/src/shapes/CPolygonManager.java
@@ -0,0 +1,34 @@
+package shapes;
+
+import java.awt.Polygon;
+
+public class CPolygonManager extends CShapeManager {
+	private static final long serialVersionUID = 1L;
+	
+	public CPolygonManager() {
+		super(EDrawingType.NPoint);
+		this.shape = new Polygon();
+	}
+	@Override
+	public void setOrgin(int x, int y) {
+		Polygon polygon = (Polygon) this.shape;
+		polygon.addPoint(x, y);
+		polygon.addPoint(x, y);
+	}
+	@Override
+	public void addPoint(int x, int y) {
+		Polygon polygon = (Polygon) this.shape;
+		polygon.addPoint(x, y);
+	}
+	@Override
+	public void movePoint(int x, int y) {
+		Polygon polygon = (Polygon) this.shape;
+		polygon.xpoints[polygon.npoints-1] = x;
+		polygon.ypoints[polygon.npoints-1] = y;
+	}
+	@Override
+	public CShapeManager newClone() {
+		// TODO Auto-generated method stub
+		return new CPolygonManager();
+	}
+}
