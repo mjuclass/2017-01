fec8b0a5ef4763e841e3c8a3f5844844dcd6f43a
choi.mjuclass <choi.mjuclass@Notebook-HP.LSIS.BIZ>
2017-04-26 10:39:15 +0900

    2017-04-26

---
+++ ProceduralProgramming/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import calculator.Calculator;
+
+public class Main {
+	public static void main(String[] args) {
+		// create object
+		Calculator calculator = new Calculator();
+		calculator.add();
+	}
+}
