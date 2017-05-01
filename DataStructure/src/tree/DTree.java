fa0c92b92632dc000c0a5304a80c8e9cc9330b29
choi.mjuclass <choi.mjuclass@Notebook-HP.LSIS.BIZ>
2017-05-01 16:14:24 +0900

    2017-05-01

--- DataStructure/src/tree/DTree.java
+++ DataStructure/src/tree/DTree.java
@@ -2,23 +2,54 @@ package tree;
 
 public class DTree {
 	private class Node {
-		public Node left;
-		public char token;
-		public Node right;		
+		private Node left;
+		private char token;
+		private Node right;
+		
+		public Node() {
+			this.left = null;
+			this.token = '\0';
+			this.right = null;
+		}
+		public Node getLeft() {	return left;}
+		public void setLeft(Node left) {this.left = left;}
+		public char getToken() {return token;}
+		public void setToken(char token) {this.token = token;}
+		public Node getRight() {return right;}
+		public void setRight(Node right) {this.right = right;}
 	}
 	
 	private Node root;
 	private String expression = "+-+abc*df";
 	private int position = 0;
 	
+	private boolean isOperator(char c) {
+		if (c=='+'||c=='-'||c=='*'||c=='/') {
+			return true;
+		}
+		return false;
+		
+	}
 	private Node makeTree() {
 		Node node = new Node();
-		
-		
-		
+		char token = expression.charAt(position);
+		node.setToken(token);
+		position++;
+		if (isOperator(token)) {
+			node.setLeft(makeTree());
+			node.setRight(makeTree());
+		}		
 		return node;
 	}
+	private void traverse(Node node) {
+		if (node==null) return;
+		
+		traverse(node.getLeft());
+		traverse(node.getRight());		
+		System.out.println(node.getToken());
+	}	
 	public void build() {
 		root = makeTree();
+		traverse(root);
 	}
 }
